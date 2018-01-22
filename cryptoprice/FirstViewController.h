c294c6f0417aca0a56910c1d179b2d1e19986bf9
williamtsuii <william_xu@live.com>
2018-01-22 14:06:03 -0800

    Initial Commit

---
+++ cryptoprice/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  cryptoprice
+//
+//  Created by william on 2018-01-22.
+//  Copyright © 2018 cpw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ cryptoprice/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  cryptoprice
+//
+//  Created by william on 2018-01-22.
+//  Copyright © 2018 cpw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
